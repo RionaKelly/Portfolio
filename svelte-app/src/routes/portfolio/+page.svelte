549ea0239c65499ef18037f1c63996becee20a9b
RionaKelly <c00296706@setu.ie>
2025-05-14 23:06:39 +0100

    portfolio in working state

--- svelte-app/src/routes/portfolio/+page.svelte
+++ svelte-app/src/routes/portfolio/+page.svelte
@@ -12,9 +12,27 @@
 </script>
 
 <div>
-<h1>Portfolio</h1>
+  <h1>Portfolio</h1>
 </div>
-      
+<div>
+  <div class="c1">
+    <img src="{base}/StarburstSS.png" alt="Screenshot of a game with a spaceship and planet in space" width=600>
+    <h2><b><a href="{base}/library">Starburst</b></h2>
+    <p>My team's entry to the 2025 Games Fleadh, Starburst, is a fast paced 2D Shooter where you grab asteroids around you and throw them at planets to destroy them and harvest their cores.</p>
+  </div>
+  <div class="c2">
+    <img src="{base}/PaintPartySS.png" alt="Screenshot of a game with 4 characters painting over an area" width=600 >
+    <h2><b><a href="{base}/library">Paint Party</b></h2>
+    <p>This is a multiplayer party game for up to four people where  you try and paint your colour on as much surface area as you can. I made this with 3 other students as a class project.</p>
+
+  </div>
+  <div class="c3">
+    <img src="{base}/temporary.png" alt="temporary placeholder" width=600>
+    <h2><b>More Soon!</b></h2>
+    <p>That's all I have in store for now but I plan to update the website whenever I have more work to show off.</p>
+  </div>
+</div>
+
 <style>
 h1 {
   grid-area: title;
@@ -29,4 +47,56 @@ p {
   padding: 16px;
 }
 
+.c1 { /* These are left as seperate classes so that I can play around and try different settings for each one */
+  float: left;
+  width: 33%;
+  padding: 16px;
+}
+.c2 {
+  float: left;
+  width: 33%;
+  padding: 16px;
+}
+.c3 {
+  float: right;
+  width: 33%;
+  padding: 16px;
+}
+.c1 p {
+  padding-left: 0;
+}
+.c2 p {
+  padding-left: 0;
+}
+.c3 p {
+  padding-left: 0;
+}
+
+/* Mobile Navigation:*/
+@media (max-width: 1200px) {
+  .right {
+    display: none;
+  }
+
+  .left {
+    width: 100%
+  }
+
+  .c1 {
+    float: none;
+    width: 100%;
+  }
+  .c2 {
+    float: none;
+    width: 100%;
+  }
+  .c3 {
+    float: none;
+    width: 100%;
+  }
+
+  .desktop-only {
+    display: none;
+  }
+}
 </style>
