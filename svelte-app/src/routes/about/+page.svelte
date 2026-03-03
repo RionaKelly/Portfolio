987c920f4a8a9b0f6d839ce3a3b9a333394bddb1
Riona <c00296706@setu.ie>
2026-03-03 22:11:48 +0000

    home page improved

--- svelte-app/src/routes/about/+page.svelte
+++ svelte-app/src/routes/about/+page.svelte
@@ -4,35 +4,27 @@
 
 <script>
 // @ts-nocheck
-    import { base } from '$app/paths';
-    import { browser } from '$app/environment';
-    import { onMount } from 'svelte';
 
-  /**
- * @type {__sveltets_2_IsomorphicComponent<__sveltets_2_PropsWithChildren<{ timingFunction?: string | undefined; arrows?: boolean | undefined; infinite?: boolean | undefined; initialPageIndex?: number | undefined; duration?: number | undefined; autoplay?: boolean | undefined; autoplayDuration?: number | undefined; autoplayDirection?: any; pauseOnFocus?: boolean | undefined; autoplayProgressVisible?: boolean | undefined; dots?: boolean | undefined; swiping?: boolean | undefined; particlesToShow?: number | undefined; particlesToScroll?: number | undefined; goTo?: ((pageIndex: any, options: any) => Promise<void>) | undefined; goToPrev?: ((options: any) => Promise<void>) | undefined; goToNext?: ((options: any) => Promise<void>) | undefined; }, { prev: { showPrevPage: any; }; default: { loaded: any[]; currentPageIndex: any; }; next: { showNextPage: any; }; dots: { currentPageIndex: any; pagesCount: number; showPage: (pageIndex: any) => Promise<void>; }; }>, { [evt: string]: CustomEvent<any>; }, { prev: { showPrevPage: any; }; default: { loaded: any[]; currentPageIndex: any; }; next: { showNextPage: any; }; dots: { currentPageIndex: any; pagesCount: number; showPage: (pageIndex: any) => Promise<void>; }; }, { goTo: (pageIndex: any, options: any) => Promise<void>; goToPrev: (options: any) => Promise<void>; goToNext: (options: any) => Promise<void>; }, string>}
- */
-  let Carousel; // for dynamic import of the carousel component
-  /* @type {import('svelte-carousel').default} */
-  let carousel; // for calling methods of the carousel instance
+  import { base } from '$app/paths';
+  import { browser } from '$app/environment';
+  import { onMount } from 'svelte';
   
-
-  onMount(async () => {
+  onMount(() => {
       console.log("Home Page Loaded");
-      if (browser) {
-          // dynamically import only in the browser
-          const module = await import('svelte-carousel');
-          Carousel = module.default;
-      }
-  });
-    
+  })
 </script>
+
 <div class="row">
 <div class="left">
   <h1>About Me</h1>
-  <p>Hello!! I'm Riona, a Web/Game Designer and Developer based in Ireland :></p>
+  <p>Hello!! I'm Riona, a Web/Game Designer & Developer based in Ireland :></p>
   <p>I'm super interested in games, art and poetry, and am always up for a challenge or to try something new.
   <br>So feel free to reach out and contact me for any sort of project or opportunity!</p>
-  <p></p>
+  <p class="eyes">
+  <img src="{base}/eyes3.jpg" alt="Riona Kelly" class="eye">
+  <img src="{base}/eyes1.jpg" alt="Riona Kelly" class="eye">
+  <img src="{base}/eyes2.jpg" alt="Riona Kelly" class="eye">
+  </p>
   <h2><b>Some Random Facts</b></h2>
   <p>- If you couldn't tell, my favourite colours are purple and yellow.</p>
   <p>- I have 3 cats</p>
@@ -40,37 +32,22 @@
   <p>- I loove socialising and will take any opportunity to meet new people (a lot of you reading this are probably random people I met while out, nice to see you again!)</p>
   <p>- I'm hypermobile/double-jointed and can bend my my body in ways that it probably shouldn't go!</p>
   <p>- I love cosplaying and you can find me at most Irish conventions if I have the money for them.</p>
+  <p class="eyes">
+  <img src="{base}/eyes3.jpg" alt="Riona Kelly" class="eye">
+  <img src="{base}/eyes1.jpg" alt="Riona Kelly" class="eye">
+  <img src="{base}/eyes2.jpg" alt="Riona Kelly" class="eye">
+  </p>
 </div>
 
-<div class="right" align="right">
-<div class="carousel">{#if Carousel}
- <Carousel
-     bind:this={carousel}
-     autoplay
-     autoplayDuration={3000}
-     pauseOnFocus
-     swiping
-     dots={false}
-    let:showPrevPage
-    let:showNextPage
- >
-     <div><center>
-      <img src="{base}/riona_red.png" alt="Riona Kelly" width=700>
-      </center></div>
-     <div><center>
-      <img src="{base}/riona_purple.png" alt="Riona Kelly" width=700>
-      </center></div>
-     <div><center>
-      <img src="{base}/riona_bus.png" alt="Riona Kelly" width=700>
-      </center></div>
- </Carousel>
-{:else}
- Fallback content while loading
- <p>Loading carousel...</p>
-{/if}</div>
+<div class="right">
+<img src="{base}/riona_red.png" alt="Riona Kelly" width=600 class="pics">
+<br>
+<img src="{base}/riona_bus.png" alt="Riona Kelly" width=600 class="pics">
 </div>
 </div>
- 
+<div>
+
+</div>
 
 <style>
 
@@ -83,6 +60,22 @@
  float: right;
  width: 50%;
  padding: 40px;
+ float: right;
+}
+
+.pics {
+  display: block;
+  margin-left: auto;
+}
+
+.eyes {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.eye {
+  width: 33%;
 }
 
 .row:after {
@@ -112,8 +105,7 @@ p {
 /* Mobile Navigation:*/
 @media (max-width: 1200px) {
   .right {
-    float:left;
-    width: 500px;
+    display: none;
   }
 
   .left {
