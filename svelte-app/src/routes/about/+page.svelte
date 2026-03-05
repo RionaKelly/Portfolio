34e8966aa25ac9104f7f7c0263c3df4dc9ce046b
Riona <c00296706@setu.ie>
2026-03-05 13:10:42 +0000

    About page formatting complete, just need content

--- svelte-app/src/routes/about/+page.svelte
+++ svelte-app/src/routes/about/+page.svelte
@@ -17,36 +17,32 @@
 <div class="row">
 <div class="left">
   <h1>About Me</h1>
-  <p>Hello!! I'm Riona, a Web/Game Designer & Developer based in Ireland :D</p>
-  <p>I'm super interested in games, art and poetry, and am always up for a challenge or to try something new.
-  <br>So feel free to reach out and contact me for any sort of project or opportunity!</p>
-  <br>
+  <p>Hello!! I'm Riona, a Web/Game Designer & Developer based in Ireland :></p>
+  <p>I'm super interested in games & art mainly, but I always love trying new things! 
+    so feel free to reach out and contact me for any sort of project or opportunity, or even if you just want to talk or hang out :D.</p>
   <p class="eyes">
   <img src="{base}/eyes3.jpg" alt="Riona Kelly" class="eye">
   <img src="{base}/eyes1.jpg" alt="Riona Kelly" class="eye">
   <img src="{base}/eyes2.jpg" alt="Riona Kelly" class="eye">
   </p>
-  <br>
   <h2><b>The Next Part</b></h2>
-  <p>Honestly I'm not sure what to put here, I don't know what people usually put in 'About Me' sections.</p>
-  <p>I also always feel weird about giving too much personal information...</p>
-  <p>If you have any questions I'm happy to answer though!</p>
-  <p>For now I guess I hope you like what I've done with the place, keep looking around and I enjoyyy!!!</p>
-  <br>
+  <p>Honestly I'm not sure what to put here, I don't know what people usually put in 'About Me' sections.
+  <br>I also always feel weird about giving too much personal information...</p>
+  <p>If you have any questions I'm happy to answer though!
+  <br>For now I guess I hope you like what I've done with the place, keep looking around and I enjoyyy!!!</p>
   <p class="eyes">
   <img src="{base}/eyes3.jpg" alt="Riona Kelly" class="eye">
   <img src="{base}/eyes1.jpg" alt="Riona Kelly" class="eye">
   <img src="{base}/eyes2.jpg" alt="Riona Kelly" class="eye">
   </p>
-  <br>
   <h2><b>Closing Statements</b></h2>
   <p>byyyeeeee</p>
 </div>
 
 <div class="right">
-<img src="{base}/riona_red.png" alt="Riona Kelly" width=600 class="pics">
-<br>
-<img src="{base}/riona_bus.png" alt="Riona Kelly" width=600 class="pics">
+<img src="{base}/riona_red.png" alt="Riona Kelly" class="pics">
+<!--<br>
+<img src="{base}/riona_bus.png" alt="Riona Kelly" width=600 class="pics">-->
 </div>
 </div>
 <div>
@@ -60,26 +56,31 @@
  width: 50%;
  padding-left: 20px;
 }
+
 .right {
  float: right;
  width: 50%;
- padding: 40px;
- float: right;
+ padding: 60px;
 }
 
 .pics {
   display: block;
   margin-left: auto;
+  border-radius: 45px;
+  max-height: 600px;
 }
 
 .eyes {
-  display: flex;
+  /* display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: center; */
+  margin: 5px;
 }
 
 .eye {
-  width: 33%;
+  width: 32%;
+  padding-right: 3px;
+  border-radius: 15px;
 }
 
 .row:after {
@@ -113,7 +114,14 @@ p {
   }
 
   .left {
-    width: 100%
+    width: 100%;
+    padding-left: 3px;
+    padding-right: 5px;
   }
+
+  .eye {
+  border-radius: 10px;
+  padding-right: 0px;
+}
 }
 </style>
